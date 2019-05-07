2df77af3c12d7f58fbac51c325dd5605d7884b42
Lino Virgen <lino.virgen96@gmail.com>
2019-05-07 14:29:20 -0500

    added count operation

--- main.js
+++ main.js
@@ -13,10 +13,8 @@ var food_average = {};
 var food_count = {};
 var water_average = {};
 var water_count = {};
-var electric_average = {};
-var electric_count = {};
-var shelter_average = {};
-var shelter_count = {};
+var electricity_average = {};
+var electricity_count = {};
 
 var s_date = new Date( "2020-4-6 14:33:00" );
 var e_date = new Date( "2020-4-6 20:30:00" );
@@ -25,36 +23,23 @@ const E_TIME = new Date("2020-04-06 14:33:00");
 
 // ------------------------------------ SLIDER ----------------------------------------------------
 
-$( function() {
+$( function()
+{
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
       max: 5606,
-      values: [ 0, 330 ],
-      slide: function( event, ui )
-      {
-          var date1 = new Date( E_TIME.getTime() );
-          var date2 = new Date( E_TIME.getTime() );
-
-          date1.setMinutes( date1.getMinutes() +  ui.values[ 0 ] );
-          date2.setMinutes( date2.getMinutes() +  ui.values[ 1 ] );
-
-          $( "#date" ).val( formatedDate(date1) + " to " + formatedDate(date2)  );
-
-          // update map visualization using data points between date1 and date2
-        //   update( date1, date2 )
-
-      }
+      values: [ 0, 330 ]
     });
 
     $( "#date" ).val( formatedDate(s_date) + " to " + formatedDate(e_date)  );
 
-} );
+    function formatedDate( date )
+    {
+        return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
+    }
 
-function formatedDate( date )
-{
-    return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
-}
+} );
 
 // ------------------------------------ HEATMAP ----------------------------------------------------
 
@@ -63,8 +48,7 @@ var overall_svg = d3.select("svg#overall_map");
 var health_svg = d3.select("svg#health_map");
 var food_svg = d3.select("svg#food_map");
 var water_svg = d3.select("svg#water_map");
-var electric_svg = d3.select("svg#electric_map");
-var shelter_svg = d3.select("svg#shelter_map");
+var electricity_svg = d3.select("svg#electricity_map");
 
 var b_width = +overall_svg.attr("width");
 var b_height = +overall_svg.attr("height");
@@ -72,9 +56,13 @@ var s_width = +health_svg.attr("width");
 var s_height = +health_svg.attr("height");
 
 // color scale
-var color = d3.scale.linear()
+var colorAverage = d3.scale.linear()
 		.domain([ -1, 0, 1 ])
         .range(["red","white","green"]);
+
+var colorCount = d3.scale.linear()
+		.domain([ 0, 150, 300 ])
+        .range(["white","blue"]);
 
 // Load external data and boot
 queue()
@@ -91,8 +79,8 @@ function ready( error, topology, data )
         d.time = new Date( d.time );
     });
 
-    console.log(topology);
-    console.log(data);
+    // console.log(topology);
+    // console.log(data);
 
     // Map and projection
     var projection = d3.geo.mercator().scale(1).translate([0, 0]);
@@ -106,7 +94,7 @@ function ready( error, topology, data )
 
     projection.scale(s).translate(t);
 
-    var overall_map = overall_svg.selectAll("overall_path").data(geojson.features)
+    var overall_map = overall_svg.selectAll("map").data(geojson.features)
         .enter()
         .append("path")
         .attr("d", path);
@@ -116,35 +104,33 @@ function ready( error, topology, data )
     
     projection.scale(s).translate(t);
 
-    var health_map = health_svg.selectAll("health_path").data(geojson.features)
+    var health_map = health_svg.selectAll("map").data(geojson.features)
         .enter()
         .append("path")
         .attr("d", path);
 
-    var food_map = food_svg.selectAll("food_path").data(geojson.features)
+    var food_map = food_svg.selectAll("map").data(geojson.features)
         .enter()
         .append("path")
         .attr("d", path);
 
-    var water_map = water_svg.selectAll("water_path").data(geojson.features)
+    var water_map = water_svg.selectAll("map").data(geojson.features)
         .enter()
         .append("path")
         .attr("d", path);
 
-    var electric_map = electric_svg.selectAll("electric_path").data(geojson.features)
+    var electricity_map = electricity_svg.selectAll("map").data(geojson.features)
         .enter()
         .append("path")
         .attr("d", path);
 
-    var shelter_map = shelter_svg.selectAll("shelter_path").data(geojson.features)
-        .enter()
-        .append("path")
-        .attr("d", path);
+    addTitle( overall_svg, b_width, 17, "16px", "Overall Resource Map" );
+    addTitle( health_svg, s_width, 10,"12px", "Health Map" );
+    addTitle( food_svg, s_width, 10,"12px", "Food Map" );
+    addTitle( water_svg, s_width, 10,"12px", "Water Map" );
+    addTitle( electricity_svg, s_width, 10,"12px", "electricity Map" );
 
-    var date1 = s_date;
-    var date2 = e_date;
-
-    update( date1, date2 )
+    update( s_date, e_date );
 
     function update( date1, date2 )
     {
@@ -163,11 +149,9 @@ function ready( error, topology, data )
             water_average[counties[i]] = 0;
             water_count[counties[i]] = 0;
 
-            electric_average[counties[i]] = 0;
-            electric_count[counties[i]] = 0;
+            electricity_average[counties[i]] = 0;
+            electricity_count[counties[i]] = 0;
 
-            shelter_average[counties[i]] = 0;
-            shelter_count[counties[i]] = 0;
         }
 
         // filtering data
@@ -200,15 +184,10 @@ function ready( error, topology, data )
 
                 if( data[i].category == "electric" )
                 {
-                    electric_average[data[i].location] += +data[i].sentiment;
-                    electric_count[data[i].location]++;
+                    electricity_average[data[i].location] += +data[i].sentiment;
+                    electricity_count[data[i].location]++;
                 }
 
-                if( data[i].category == "shelter" )
-                {
-                    shelter_average[data[i].location] += +data[i].sentiment;
-                    shelter_count[data[i].location]++;
-                }
             }
         }
 
@@ -227,40 +206,51 @@ function ready( error, topology, data )
             if( water_average[c] != 0 )
                 water_average[c] = water_average[c] / water_count[c];
 
-            if( electric_average[c] != 0 )
-                electric_average[c] = electric_average[c] / electric_count[c];
+            if( electricity_average[c] != 0 )
+                electricity_average[c] = electricity_average[c] / electricity_count[c];
 
-            if( shelter_average[c] != 0 )
-                shelter_average[c] = shelter_average[c] / shelter_count[c];
         }
 
         // updating maps
-        overall_map.style("fill", function(d) {
-            return color(overall_average[d.properties.Nbrhood])
-        });
+        updateMaps( overall_map, overall_average, overall_count );
+        updateMaps( health_map, health_average, health_count );
+        updateMaps( food_map, food_average, food_count );
+        updateMaps( water_map, water_average, water_count );
+        updateMaps( electricity_map, electricity_average, electricity_count );
 
-        health_map.style("fill", function(d) {
-            return color(health_average[d.properties.Nbrhood])
-        });
+        function updateMaps( map, average, count )
+        {
+            var operation = document.getElementById("operation").value;
 
-        food_map.style("fill", function(d) {
-            return color(food_average[d.properties.Nbrhood])
-        });
+            if( operation == "average" )
+            {
+                map.style("fill", function(d) {
+                    return colorAverage(average[d.properties.Nbrhood])
+                });
+            }
+            else
+            {
+                map.style("fill", function(d) {
+                    return colorCount(count[d.properties.Nbrhood])
+                });
+            }
 
-        water_map.style("fill", function(d) {
-            return color(water_average[d.properties.Nbrhood])
-        });
+        }
 
-        electric_map.style("fill", function(d) {
-            return color(electric_average[d.properties.Nbrhood])
-        });
-
-        shelter_map.style("fill", function(d) {
-            return color(shelter_average[d.properties.Nbrhood])
-        });
     }
 
-    $( function() {
+    function addTitle( svg, w, h, size, text )
+    {
+        svg.append("text")
+            .attr("x", w / 2 )             
+            .attr("y", h )
+            .style("text-anchor", "middle")
+            .style("font-size", size) 
+            .text( text );
+    }
+
+    $( function()
+    {
         $( "#slider-range" ).slider({
           range: true,
           min: 0,
@@ -283,13 +273,13 @@ function ready( error, topology, data )
         });
     
         $( "#date" ).val( formatedDate(s_date) + " to " + formatedDate(e_date)  );
+
+        function formatedDate( date )
+        {
+            return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
+        }
     
     } );
-    
-    function formatedDate( date )
-    {
-        return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
-    }
     
 }
 
