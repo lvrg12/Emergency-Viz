755c0904524c8ceedc50ca20fc0a04db28cc8735
Lino Virgen <lino.virgen96@gmail.com>
2019-05-06 21:24:46 -0500

    simplify map

--- main.js
+++ main.js
@@ -45,54 +45,79 @@ function formatedDate( date )
     return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
 }
 
-
 // ------------------------------------ HEATMAP ----------------------------------------------------
 
-(function(){
+// The svg
+var svg = d3.select("svg");
+var width = +svg.attr("width");
+var height = +svg.attr("height");
 
-let width = 600,
-    height = 500;
-let projection = d3.geo.mercator()
-    .scale(1)
-    .translate([0, 0]);
-let path = d3.geo.path()
-    .projection(projection);
-let svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-let url = "https://raw.githubusercontent.com/HXDU/Project-3-Mini-Challenge-3/master/WebCode/StHimark.topojson";
-let tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-d3.json(url, function (error, topology) {
-    if (error) throw error;
-    let geojson = topojson.feature(topology, topology.objects.StHimark);
-    let b = path.bounds(geojson),
-        s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
-        t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
-    projection
-        .scale(s)
-        .translate(t);
-    svg.selectAll("path")
-        .data(geojson.features)
-        .enter().append("path")
+// color scale
+// var data = d3.map();
+// var colorScale = d3.scaleThreshold()
+//   .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+//   .range(d3.schemeBlues[7]);
+
+// Map and projection
+var projection = d3.geo.mercator().scale(1).translate([0, 0]);
+var path = d3.geo.path().projection(projection);
+var url = "https://raw.githubusercontent.com/HXDU/Project-3-Mini-Challenge-3/master/WebCode/StHimark.topojson";
+var tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+
+d3.json(url, function (error, topology)
+{
+    if (error)
+        throw error;
+
+    var geojson = topojson.feature(topology, topology.objects.StHimark);
+    var b = path.bounds(geojson);
+    var s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+    var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+    projection.scale(s).translate(t);
+
+    svg.selectAll("path").data(geojson.features)
+        .enter()
+        .append("path")
         .attr("d", path)
-        .style("fill","red")
-        .style("opacity", Math.random )
-        .on("mouseover", function (d) {
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-            tooltip.html(d.properties.Nbrhood)
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function (d) {
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
+        // .on("mouseover", function (d){
+        //     tooltip.transition()
+        //         .duration(200)
+        //         .style("opacity", .9);
+        //     tooltip.html(d.properties.Nbrhood)
+        //         .style("left", (d3.event.pageX) + "px")
+        //         .style("top", (d3.event.pageY - 28) + "px");
+        //     })
+        // .on("mouseout", function (d) {
+        //     tooltip.transition()
+        //         .duration(500)
+        //         .style("opacity", 0);
+        //     });
 });
 
 
-})();
+// Load external data and boot
+d3.queue()
+  .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+  .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) {  })
+  .await(ready);
+
+function ready(error, topo)
+{
+//   // Draw the map
+//   svg.append("g")
+//     .selectAll("path")
+//     .data(topo.features)
+//     .enter()
+//     .append("path")
+//       // draw each country
+//       .attr("d", d3.geoPath()
+//         .projection(projection)
+//       )
+//       // set the color of each country
+//       .attr("fill", function (d) {
+//         d.total = data.get(d.id) || 0;
+//         return colorScale(d.total);
+//       });
+}
+
