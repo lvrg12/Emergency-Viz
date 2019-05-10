9f2c598b3400b148a52b182a69e2a50976ea08e7
ak65234 <6294860+ak65234@users.noreply.github.com>
2019-05-10 11:13:03 -0500

    Strict mode, function wrapper, and let

--- lineGraph.js
+++ lineGraph.js
@@ -1,79 +1,84 @@
-var margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = 900 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+"use strict";
 
-var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
+(function () {
+    let margin = { top: 20, right: 80, bottom: 30, left: 50 },
+        width = 900 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-
-var x = d3.time.scale()
-    .range([0,width]);
-
-var y = d3.scale.linear()
-    .range([height,0]);
-
-var color = d3.scale.category10();
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d) { return x(d.time); })
-    .y(function(d) { return y(d.sentiment); });
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.csv("data/test.csv", function(error, data) {
+    let parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
 
 
-    color.domain(d3.keys(data[0]).filter(function(key) { return key == "category"; }));
+    let x = d3.time.scale()
+        .range([0, width]);
 
-    // format data
+    let y = d3.scale.linear()
+        .range([height, 0]);
 
-    data = data.map( function (d) {
-        return {
-            category: d.category,
-            time: parseDate(d.time),
-            sentiment: +d.sentiment };
+    let color = d3.scale.category10();
+
+    let xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    let yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    let line = d3.svg.line()
+        .interpolate("basis")
+        .x(function (d) { return x(d.time); })
+        .y(function (d) { return y(d.sentiment); });
+
+    let svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("data/test.csv", function (error, data) {
+
+
+        color.domain(d3.keys(data[0]).filter(function (key) { return key == "category"; }));
+
+        // format data
+
+        data = data.map(function (d) {
+            return {
+                category: d.category,
+                time: parseDate(d.time),
+                sentiment: +d.sentiment
+            };
+        });
+
+
+        // nest the data on category since we want to only draw one line per category
+        data = d3.nest().key(function (d) { return d.category; }).entries(data);
+
+
+        x.domain([d3.min(data, function (d) { return d3.min(d.values, function (d) { return d.time; }); }),
+        d3.max(data, function (d) { return d3.max(d.values, function (d) { return d.time; }); })]);
+        y.domain([d3.min(data, function (d) { return d3.min(d.values, function (d) { return d.sentiment; }); }),
+        d3.max(data, function (d) { return d3.max(d.values, function (d) { return d.sentiment; }); })]);
+
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        let categories = svg.selectAll(".category")
+            .data(data, function (d) { return d.key; })
+            .enter().append("g")
+            .attr("class", "category");
+
+        categories.append("path")
+            .attr("class", "line")
+            .attr("d", function (d) { return line(d.values); })
+            .style("stroke", function (d) { return color(d.key); });
+
     });
-
-
-    // nest the data on category since we want to only draw one line per category
-    data = d3.nest().key(function(d) { return d.category; }).entries(data);
-
-
-    x.domain([d3.min(data, function(d) { return d3.min(d.values, function (d) { return d.time; }); }),
-        d3.max(data, function(d) { return d3.max(d.values, function (d) { return d.time; }); })]);
-    y.domain([d3.min(data, function(d) { return d3.min(d.values, function (d) { return d.sentiment; }); }),
-        d3.max(data, function(d) { return d3.max(d.values, function (d) { return d.sentiment; }); })]);
-
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-
-    var categories = svg.selectAll(".category")
-        .data(data, function(d) { return d.key; })
-        .enter().append("g")
-        .attr("class", "category");
-
-    categories.append("path")
-        .attr("class", "line")
-        .attr("d", function(d) { return line(d.values); })
-        .style("stroke", function(d) { return color(d.key); });
-
-});
+})();
--- stacked.js
+++ stacked.js
@@ -1,72 +1,76 @@
-var format = d3.time.format("%Y-%m-%d %H:%M:%S");
+"use strict";
 
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+(function () {
+    let format = d3.time.format("%Y-%m-%d %H:%M:%S");
 
-var x = d3.time.scale()
-    .range([0, width]);
+    let margin = { top: 20, right: 30, bottom: 30, left: 40 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+    let x = d3.time.scale()
+        .range([0, width]);
 
-var z = d3.scale.category20c();
+    let y = d3.scale.linear()
+        .range([height, 0]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+    let z = d3.scale.category20c();
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+    let xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
-var stack = d3.layout.stack()
-    .offset("zero")
-    .values(function(d) { return d.values; })
-    .x(function(d) { return d.time; })
-    .y(function(d) { return d.sentiment; });
+    let yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
 
-var nest = d3.nest()
-    .key(function(d) { return d.category; });
+    let stack = d3.layout.stack()
+        .offset("zero")
+        .values(function (d) { return d.values; })
+        .x(function (d) { return d.time; })
+        .y(function (d) { return d.sentiment; });
 
-var area = d3.svg.area()
-    .interpolate("cardinal")
-    .x(function(d) { return x(d.time); })
-    .y0(function(d) { return y(d.y0); })
-    .y1(function(d) { return y(d.y0 + d.y); });
+    let nest = d3.nest()
+        .key(function (d) { return d.category; });
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    let area = d3.svg.area()
+        .interpolate("cardinal")
+        .x(function (d) { return x(d.time); })
+        .y0(function (d) { return y(d.y0); })
+        .y1(function (d) { return y(d.y0 + d.y); });
 
-d3.csv("data/test.csv", function(error, data) {
-    if (error) throw error;
+    let svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    data.forEach(function(d) {
-        d.time = format.parse(d.time);
-        d.sentiment = +d.sentiment;
+    d3.csv("data/test.csv", function (error, data) {
+        if (error) throw error;
+
+        data.forEach(function (d) {
+            d.time = format.parse(d.time);
+            d.sentiment = +d.sentiment;
+        });
+
+        let layers = stack(nest.entries(data));
+
+        x.domain(d3.extent(data, function (d) { return d.time; }));
+        y.domain([0, d3.max(data, function (d) { return d.y0 + d.y; })]);
+
+        svg.selectAll(".layer")
+            .data(layers)
+            .enter().append("path")
+            .attr("class", "layer")
+            .attr("d", function (d) { return area(d.values); })
+            .style("fill", function (d, i) { return z(i); });
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
     });
-
-    var layers = stack(nest.entries(data));
-
-    x.domain(d3.extent(data, function(d) { return d.time; }));
-    y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
-
-    svg.selectAll(".layer")
-        .data(layers)
-        .enter().append("path")
-        .attr("class", "layer")
-        .attr("d", function(d) { return area(d.values); })
-        .style("fill", function(d, i) { return z(i); });
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-});
+})();
